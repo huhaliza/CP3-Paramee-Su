e9cbb603c59956615a1a278a6b0604d6498c3e63
Paramee <huhaliza@gmail.com>
2021-03-31 17:41:30 +0700

    add file

---
+++ Exercise4_Paramee_S.py
@@ -0,0 +1,10 @@
+FE = 80.5   #Foundation English
+GB = 90.3   #General Business
+ICS = 70    #Introduction to Computer Systems
+CP = 80     #Computer Programming
+print("--- Your Score ---")
+print("Foundation English : คะแนนที่ได้", FE)
+print("General Business : คะแนนที่ได้", GB)
+print("Introduction to Computer Systems : คะแนนที่ได้", ICS)
+print("Computer Programming : คะแนนที่ได้", CP)
+
